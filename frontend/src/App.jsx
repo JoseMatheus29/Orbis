d80d4c9d0a8bc7eebd104a288ae8ac3a370c4d8a
Alairton Junior <alairton20@gmail.com>
2023-04-26 23:01:04 -0300

    Configurando ambiente de desenvolvimento front-end
    Testado requisito funcional básico e configurando o json-server para trabalhar com informações dinâmicas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { api } from './services/api'
+import { AutoComplete } from "primereact/autocomplete";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState([]);
+  const [filteredItems, setFilteredItems] = useState(null);
+  const [ stage, setStage ] = useState({});
+
+  useEffect(() => {
+    api('/tools')
+      .then((response) => {
+        const newItems = response.data;
+        setItems(newItems);
+      })
+  }, []);
+
+  useEffect(() => {
+
+    if(selectedItem.name) {
+      api(`/stages?id=${selectedItem.stage_id}`)
+       .then((response) => {
+      const newStage = response.data;
+      setStage(newStage[0]);
+      console.log(stage)
+      })
+    }
+  }, [selectedItem])
+
+  const searchItems = (event) => {
+    //in a real application, make a request to a remote url with the query and return filtered results, for demo purposes we filter at client side
+    let query = event.query;
+    let _filteredItems = [];
+
+    for(let i = 0; i < items.length; i++) {
+        let item = items[i];
+        if (item.name.toLowerCase().indexOf(query.toLowerCase()) === 0) {
+            _filteredItems.push(item);
+        }
+    }
+
+    setFilteredItems(_filteredItems);
+  }
+
+
 
   return (
     <>
+      
+      <div className="flex">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Selecione a ferramenta</h1>
+        <AutoComplete value={selectedItem} suggestions={filteredItems} completeMethod={searchItems} placeholder='Ferramenta'
+            virtualScrollerOptions={{ itemSize: 38 }} field="name" dropdown onChange={(e) => setSelectedItem(e.value)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>Ferramenta: {selectedItem.name}</h2>
+        <p>Tipo: {selectedItem.type}</p>
+        <p>Etapa: {stage.name}</p>
+        <p>A etapa de {stage.name}: {stage.description}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
