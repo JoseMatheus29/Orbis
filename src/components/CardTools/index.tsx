44f2f501c6b6f811ce5e8788543cbdd3062e3a21
JoseMatheus29 <mateus.alvino.101@gmail.com>
2023-07-08 22:24:57 -0300

    Update Page Tools

--- src/components/CardTools/index.tsx
+++ src/components/CardTools/index.tsx
@@ -1,19 +1,23 @@
 import Time from '../../assets/icon/time.svg'
 import Halter from '../../assets/icon/halter.svg'
 import { Container, Icons  } from './styles';
+import { stageName } from "./utils";
+
 
 interface ISValueCard {
     valueTime:  number ;
     valueEffort:  number;
     variant: string;
+    stage_id: string;
 }
 
-const CardTool = ({valueTime,valueEffort, variant}: ISValueCard) => {
-    
+const CardTool = ({valueTime,valueEffort, variant, stage_id}: ISValueCard) => {
+    const stage = stageName(stage_id);
+
     return(
-        
         <Container
         variant = {variant}>
+            <div id="StageCard">{stage}</div>
             <Icons>
                 <h4>{"Tempo"}</h4>
                 {/*@ts-ignore*/}
@@ -32,6 +36,7 @@ const CardTool = ({valueTime,valueEffort, variant}: ISValueCard) => {
                 {/*@ts-ignore*/}
                 <img src={Halter} style={  valueEffort > 2 ? { opacity: 1} : {opacity: 0.5}} />
             </Icons>
+            
         </Container>
     )
 }
